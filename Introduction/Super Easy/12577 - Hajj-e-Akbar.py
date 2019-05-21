de2b2d13a466b461fe931252837ac8200c86c6e7
PrachiAG <prachiagarwal30111998@gmail.com>
2019-05-21 08:27:44 -0700

    Create  12577 - Hajj-e-Akbar.py

---
+++ Introduction/Super Easy/12577 - Hajj-e-Akbar.py
@@ -0,0 +1,11 @@
+k=1
+while(True):
+    s=input()
+    if s=="*":
+        break
+    if s=="Hajj":
+        print("Case",str(k)+":","Hajj-e-Akbar")
+        k=k+1
+    else:
+        print("Case", str(k) + ":", "Hajj-e-Asghar")
+        k=k+1
